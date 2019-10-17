bc2ca258741a7d0934b278462a0ab2774f8a6abb
Filip Matzner <floop@floop.cz>
2019-10-17 20:18:45 +0200

    Build archlinuxarm-dev without context

--- dockerfiles/archlinuxarm/build.sh
+++ dockerfiles/archlinuxarm/build.sh
@@ -4,16 +4,20 @@
 
 URL="http://os.archlinuxarm.org/os"
 IMAGE_TAR="ArchLinuxARM-rpi-2-latest.tar.gz"
+TODAY="$(date '+%Y-%m-%d')"
 
+# Download and unpack the base system tarball from archlinuxarm.org.
 rm -rf "${IMAGE_TAR}" arch-rootfs
 wget "${URL}/${IMAGE_TAR}"
 mkdir -p arch-rootfs
 tar -xzf ${IMAGE_TAR} --strip-components=1 --directory=./arch-rootfs
 
-today="$(date '+%Y-%m-%d')"
-docker build -f archlinuxarm -t "iterait/archlinuxarm:${today}" -t "iterait/archlinuxarm:latest" --squash .
-docker build -f archlinuxarm-dev -t "iterait/archlinuxarm-dev:${today}" -t "iterait/archlinuxarm-dev:latest" .
-docker push archlinuxiterait/archlinuxarm:latest
-docker push archlinuxiterait/archlinuxarm:${today}
-docker push archlinuxiterait/archlinuxarm-dev:latest
-docker push archlinuxiterait/archlinuxarm-dev:${today}
+# Build the base image.
+docker build -f archlinuxarm -t "iterait/archlinuxarm:${TODAY}" -t "iterait/archlinuxarm:latest" --squash .
+docker push iterait/archlinuxarm:latest
+docker push iterait/archlinuxarm:${TODAY}
+
+# Build the -dev version without context.
+docker build - -t "iterait/archlinuxarm-dev:${TODAY}" -t "iterait/archlinuxarm-dev:latest" < archlinuxarm-dev
+docker push iterait/archlinuxarm-dev:latest
+docker push iterait/archlinuxarm-dev:${TODAY}
