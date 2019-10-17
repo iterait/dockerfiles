cbae249588956e4c61f0a6f8b220a22c437aa059
Filip Matzner <floop@floop.cz>
2019-10-17 18:53:40 +0200

    Rename image to archlinuxarm-dev

--- dockerfiles/archlinuxarm/build.sh
+++ dockerfiles/archlinuxarm/build.sh
@@ -12,4 +12,4 @@ fi
 mkdir -p arch-rootfs
 tar -xzf ${IMAGE_TAR} --strip-components=1 --directory=./arch-rootfs
 
-docker build -f Dockerfile -t "iterait/archlinuxarm:$(date '+%Y-%m-%d')" -t "iterait/archlinuxarm:latest" --squash .
+docker build -f Dockerfile -t "iterait/archlinuxarm-dev:$(date '+%Y-%m-%d')" -t "iterait/archlinuxarm-dev:latest" --squash .
