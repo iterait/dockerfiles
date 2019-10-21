0be7d6044c1b732160998fdf945d980252796d3f
Filip Matzner <floop@floop.cz>
2019-10-21 17:25:04 +0200

    s/tag/imgname

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 
 TODAY="$(date '+%Y-%m-%d')"
-for tag in archlinux archlinux-dev tensorflow; do
-    sudo docker build -f Dockerfile.${tag} -t "iterait/${tag}:${TODAY}" -t "iterait/${tag}:latest" --squash .
+for imgname in archlinux archlinux-dev tensorflow; do
+    sudo docker build -f Dockerfile.${imgname} -t "iterait/${imgname}:${TODAY}" -t "iterait/${imgname}:latest" --squash .
     if [ "${CIRCLE_BRANCH}" == "master" ]; then
-        sudo docker push iterait/${tag}:latest
-        sudo docker push iterait/${tag}:"${TODAY}"
+        sudo docker push iterait/${imgname}:latest
+        sudo docker push iterait/${imgname}:"${TODAY}"
     fi
 done
