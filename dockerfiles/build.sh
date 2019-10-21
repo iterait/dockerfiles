688fcd7c669d634b595308c71eee30649a55ce1f
Filip Matzner <floop@floop.cz>
2019-10-21 21:28:37 +0200

    Build also cuda images
    Closes #37

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -3,8 +3,11 @@
 TODAY="$(date '+%Y-%m-%d')"
 for imgname in archlinux archlinux-dev tensorflow; do
     sudo docker build -f Dockerfile.${imgname} -t "iterait/${imgname}:${TODAY}" -t "iterait/${imgname}:latest" --squash .
+    sudo docker build -f Dockerfile.${imgname} --build-arg tag="cuda" -t "iterait/${imgname}:cuda_${TODAY}" -t "iterait/${imgname}:cuda" --squash .
     if [ "${CIRCLE_BRANCH}" == "master" ]; then
         sudo docker push iterait/${imgname}:latest
         sudo docker push iterait/${imgname}:"${TODAY}"
+        sudo docker push iterait/${imgname}:cuda
+        sudo docker push iterait/${imgname}:"cuda_${TODAY}"
     fi
 done
